295f5bb5dac29b72367f1b50cbf02681a632a578
vangjv <jonathan.vang@gmail.com>
2023-11-06 00:57:00 -0600

    removed mouseover 3dmodel and +favicon

--- src/app/services/three-dimensional-model.service.ts
+++ src/app/services/three-dimensional-model.service.ts
@@ -30,6 +30,21 @@ export class ThreeDimensionalModelService {
     });
   }
 
+  updateModelPosition(modelEntity:any, screenX:any, screenY:any) {
+    let mousePosition = new Cesium.Cartesian2(screenX, screenY);
+    let ellipsoid = this.viewer.scene.globe.ellipsoid;
+    let cartesian = this.viewer.camera.pickEllipsoid(mousePosition, ellipsoid);
+
+    if (cartesian) {
+        let cartographic = ellipsoid.cartesianToCartographic(cartesian);
+        let longitude = Cesium.Math.toDegrees(cartographic.longitude);
+        let latitude = Cesium.Math.toDegrees(cartographic.latitude);
+        modelEntity.position = Cesium.Cartesian3.fromDegrees(longitude, latitude);
+        modelEntity.position = this.viewer.scene.clampToHeight(Cesium.Cartesian3.fromDegrees(longitude, latitude), [modelEntity]);
+    }
+}
+
+
   enableAdding3DModel(){
     const add3DModelBtn = document.getElementById("add3DModelBtn");
     if (add3DModelBtn)
@@ -52,24 +67,28 @@ export class ThreeDimensionalModelService {
         model: {
           uri: "./assets/3dmodels/meatmarket.glb"
         },
-        heighReference:Cesium.HeightReference.CLAMP_TO_GROUND
+        clampToGround: true
+        // heightReference:Cesium.HeightReference.CLAMP_TO_GROUND
       });
 
-      let mouseOverModelHandler = new Cesium.ScreenSpaceEventHandler(this.viewer.scene.canvas);
-      mouseOverModelHandler.setInputAction((movement:any) => {
-        const cartesian = this.viewer.camera.pickEllipsoid(
-          movement.endPosition,
-          this.viewer.scene.globe.ellipsoid
-        );
-        if (cartesian) {
-          const cartographic = Cesium.Cartographic.fromCartesian(
-            cartesian
-          );
-          const objectsToExclude = [mouseOverEntity];
-          //entity.position = cartesian;
-          mouseOverEntity.position = this.viewer.scene.clampToHeight(cartesian, objectsToExclude);
-        }
-      }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+      // let mouseOverModelHandler = new Cesium.ScreenSpaceEventHandler(this.viewer.scene.canvas);
+      // mouseOverModelHandler.setInputAction((movement:any) => {
+      //   if (this.cesiumService.adding3DModelState() == true) {
+      //     // const cartesian = this.viewer.camera.pickEllipsoid(
+      //     //   movement.endPosition,
+      //     //   this.viewer.scene.globe.ellipsoid
+      //     // );
+      //     // if (cartesian) {
+      //     //   const cartographic = Cesium.Cartographic.fromCartesian(
+      //     //     cartesian
+      //     //   );
+      //     //   const objectsToExclude = [mouseOverEntity];
+      //     //   //entity.position = cartesian;
+      //     //   mouseOverEntity.position = this.viewer.scene.clampToHeight(cartesian, objectsToExclude);
+      //     // }
+      //     this.updateModelPosition(mouseOverEntity, movement.endPosition.x, movement.endPosition.y);
+      //   }
+      // }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
 
       let clickPlaceHandler = new Cesium.ScreenSpaceEventHandler(this.viewer.canvas);
       clickPlaceHandler.setInputAction((event:any)=> {
@@ -81,7 +100,7 @@ export class ThreeDimensionalModelService {
             earthPosition = this.viewer.scene.pickPosition(event.position);
           }
           if (Cesium.defined(earthPosition)) {
-            const heading = Cesium.Math.toRadians(135);
+            const heading = Cesium.Math.toRadians(90);
             const pitch = 0;
             const roll = 0;
             const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll);
@@ -100,7 +119,7 @@ export class ThreeDimensionalModelService {
               // heighReference:Cesium.HeightReference.RELATIVE_TO_GROUND
               heighReference:Cesium.HeightReference.CLAMP_TO_GROUND
             });
-            mouseOverModelHandler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE)
+            //mouseOverModelHandler.removeInputAction(Cesium.ScreenSpaceEventType.MOUSE_MOVE)
             this.viewer.entities.remove(mouseOverEntity);
             this.cesiumService.adding3DModelState.set(false);
           }
