6809cd12f2206787bef6b96561649eb3a6324907
vangjv <jonathan.vang@gmail.com>
2023-11-12 21:31:17 -0600

    modified prod environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://openmarketguideapi.azurewebsites.net/api/'
+  production: true,
+  apiUrl: 'https://omg-api-openmarketguide-dev.apps.sandbox-m3.1530.p1.openshiftapps.com/api/'
 };
